cec425e76505d37b4fdd679f3e96638fbe9069eb
rishithkethireddy <rreddy189@gmail.com>
2021-11-28 14:26:29 +0530

    new function int exponent power added

--- math.py
+++ math.py
@@ -13,7 +13,7 @@ def multiply(x,y):
 # Divide implementation
 def divide(x,y):
 	if y==0:
-		return DIVIDE_BY_ZERO_ERROR
+		return 'DIVIDE_BY_ZERO_ERROR'
 	else:
 		return x/y
 
@@ -23,3 +23,13 @@ def absol(x):
 		return x
 	else:
 		return -x
+
+# exponent implementation
+def power(x,a):
+	if type(a) == 'int':
+		ans = 1
+		for _ in range(1,a+1):
+			ans *= x
+		return ans
+	else:
+		return 'TYPE_ERROR a is not int'
